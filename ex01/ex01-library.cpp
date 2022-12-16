3390f5386675aebd438080d489a530f91bd77140
Zahed <zahedm45@gmail.com>
2022-12-16 12:59:35 +0100

    Move if King

--- ex01/ex01-library.cpp
+++ ex01/ex01-library.cpp
@@ -1,24 +1,111 @@
 #include <iostream>
 #include "ex01-library.h"
-
+int slope(int x, int y);
 using namespace std;
 
 // Task 1(a).  Implement this function
 Square **createChessboard(unsigned int m, unsigned int n) {
     // Replace the following with your code
-    return nullptr;
+
+    int row = m, column = n;
+
+    Square **squares = new Square*[row];
+
+    for (int i = 0; i < row; ++i) {
+        squares[i] = new Square[column];
+    }
+
+
+    for (int i = 0; i < row; ++i) {
+        for (int j = 0; j < column; ++j) {
+            squares[i][j].piece = none;
+            squares[i][j].team = nobody;
+        }
+    }
+    
+    
+    return squares;
 }
 
-// Task 1(b).  Implement this function
+// Task 1(b).  Implement this functions
 void displayChessboard(Square **c,
                        unsigned int m, unsigned int n) {
     // Write your code here
+
+
+    for (int i = 0; i < m; ++i) {
+        for (int j = 0; j < n; ++j) {
+
+            Square curr = c[i][j];
+            if (curr.piece == none) {
+                cout << "_";
+
+            } else {
+
+                if (curr.team == black && curr.piece == king) {
+                    cout << "K";
+
+                } else if (curr.team == white && curr.piece == king) {
+                    cout << "k";
+
+                } else if (curr.team == black && curr.piece == queen) {
+                    cout << "Q";
+
+                } else if (curr.team == white && curr.piece == queen) {
+                    cout << "q";
+                }
+
+            }
+            cout << " ";
+        }
+        cout << endl;
+    }
+
 }
 
 // Task 1(c).  Implement this function
 bool move(Square **c, unsigned int m, unsigned int n,
           int r1, int c1, int r2, int c2) {
     // Replace the following with your code
+
+    //if (r1 > m || c1 > n || r2 > m || c2 > n) return false;
+
+
+    Square source = c[r1][c1];
+    Square dest = c[r2][c2];
+
+
+    if (source.piece == none) {
+        return false;
+    }
+
+    if (source.team == dest.team) {
+        return false;
+    }
+
+    if (source.piece == king) {
+
+        int delta_y = abs(r1 - r2);
+        int delta_x = abs(c1 - c2);
+        int total_delta = delta_x + delta_y;
+
+
+        if (slope(delta_x, delta_y) == 0) {
+            if (total_delta > 1) return false;
+
+        } else {
+            if (total_delta > 2) return false;
+        }
+
+        c[r2][c2].piece = king;
+        c[r2][c2].team = source.team;
+
+        c[r1][c1].piece = none;
+        return true;
+    }
+
+
+
     return false;
 }
 
@@ -35,4 +122,15 @@ void deleteChessboard(Square **c, unsigned int m) {
         delete[] c[i];
     }
     delete[] c;
+}
+
+int slope(int x, int y) {
+    if (y == 0) return 0;
+    double val = (double ) y/x;
+    int val_int = (int ) val;
+    if (val - val_int == 0.0) {
+        return val_int;
+    }
+
+    return 0;
 }
--- ex01/ex01-main.cpp
+++ ex01/ex01-main.cpp
@@ -3,42 +3,52 @@
 using namespace std;
 
 int main() {
-    cout << "hello" << endl;
-    Square **c = createChessboard(3, 4);
-    c[0][0] = {queen, black};
+    int m = 5, n = 5;
+    Square **c = createChessboard(m, n);
+/*    c[0][0] = {queen, black};
     c[0][3] = {king, black};
     c[2][0] = {king, white};
-    c[1][2] = {queen, white};
+    c[1][2] = {queen, white};*/
+    c[2][2] = {king, black};
+
     cout << "Chessboard:" << endl;
-    displayChessboard(c, 3, 4);
+    displayChessboard(c, m, n);
+    bool val = move(c, m, n, 2, 2, 4, 2);
+    cout << val << endl;
 
-    cout << "Is the piece in (0,0) threatened? ";
-    if (threatened(c, 3, 4, 0, 0)) { cout << "Yes!" << endl; }
-    else { cout << "No!" << endl; }
+    cout << "Chessboard:" << endl;
+    displayChessboard(c, m, n);
 
-    cout << "Can we move from (0,0) to (0,3)? ";
-    if (move(c, 3, 4, 0, 0, 0, 3)) { cout << "Yes!" << endl; }
-    else { cout << "No!" << endl; }
 
-    cout << "Can we move from (0,0) to (0,2)? ";
-    if (move(c, 3, 4, 0, 0, 0, 2)) { cout << "Yes!" << endl; }
-    else { cout << "No!" << endl; }
 
-    cout << "Can we move from (2,0) to (2,2)? ";
-    if (move(c, 3, 4, 2, 0, 2, 2)) { cout << "Yes!" << endl; }
-    else { cout << "No!" << endl; }
 
-    cout << "Can we move from (1,2) to (0,3)? ";
-    if (move(c, 3, 4, 1, 2, 0, 3)) { cout << "Yes!" << endl; }
-    else { cout << "No!" << endl; }
+    /* cout << "Is the piece in (0,0) threatened? ";
+     if (threatened(c, 3, 4, 0, 0)) { cout << "Yes!" << endl; }
+     else { cout << "No!" << endl; }
 
-    cout << endl << "The chessboard is now:" << endl;
-    displayChessboard(c, 3, 4);
+     cout << "Can we move from (0,0) to (0,3)? ";
+     if (move(c, 3, 4, 0, 0, 0, 3)) { cout << "Yes!" << endl; }
+     else { cout << "No!" << endl; }
 
-    cout << "Is the piece in (2,0) threatened? ";
-    if (threatened(c, 3, 4, 2, 0)) { cout << "Yes!" << endl; }
-    else { cout << "No!" << endl; }
+     cout << "Can we move from (0,0) to (0,2)? ";
+     if (move(c, 3, 4, 0, 0, 0, 2)) { cout << "Yes!" << endl; }
+     else { cout << "No!" << endl; }
 
-    deleteChessboard(c, 3);
+     cout << "Can we move from (2,0) to (2,2)? ";
+     if (move(c, 3, 4, 2, 0, 2, 2)) { cout << "Yes!" << endl; }
+     else { cout << "No!" << endl; }
+
+     cout << "Can we move from (1,2) to (0,3)? ";
+     if (move(c, 3, 4, 1, 2, 0, 3)) { cout << "Yes!" << endl; }
+     else { cout << "No!" << endl; }
+
+     cout << endl << "The chessboard is now:" << endl;
+     displayChessboard(c, 3, 4);
+
+     cout << "Is the piece in (2,0) threatened? ";
+     if (threatened(c, 3, 4, 2, 0)) { cout << "Yes!" << endl; }
+     else { cout << "No!" << endl; }
+
+     deleteChessboard(c, 3);*/
     return 0;
 }
